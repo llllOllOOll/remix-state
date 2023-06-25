b7ec2916ecd1553ea9ca8ccc509e79d8f6b89d8d
Seven <7b37b3@gmail.com>
2023-06-24 21:13:51 -0300

    julia todos

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -1,39 +1,71 @@
-import { json, type LinksFunction, type LoaderArgs } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
-import { getSession } from "~/sessions";
-import styles from "~/styles/global.css";
-
-export type Todo = {
-  id: string;
-  title: string;
-  done: string;
-};
-
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: styles }];
-};
+import { Form, useLoaderData, useNavigation } from "@remix-run/react";
+import { type ActionArgs, json } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
+import { createTodo, deleteTodo, getTodos } from "~/todos.server";
+import { useRef } from "react";
+import { useFetcher } from "react-router-dom";
 
 export async function loader({ request }: LoaderArgs) {
-  const session = await getSession(request.headers.get("Cookie"));
-  const data: Todo[] = session.get("todos") || [];
-
-  return json(data);
+  const todos = await getTodos(request);
+  return json(todos);
 }
 
-export default function TodosListPage() {
-  const data = useLoaderData<typeof loader>();
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const { _action, ...values } = Object.fromEntries(formData);
+
+  if (_action === "delete") {
+    return await deleteTodo(request, values);
+  }
+
+  return await createTodo(request, values);
+}
+
+export default function TodosPage() {
+  const todos = useLoaderData<typeof loader>();
+  const fetcher = useFetcher();
+
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    fetcher.submit(e.target.name, e.target.checked);
+  };
   return (
-    <main className="">
-      <Outlet />
-      <div>
-        <ul>
-          {data.map((todo) => (
+    <div>
+      <Form method="post">
+        <fieldset>
+          <input type="hidden" name="id" />
+          <input type="hidden" name="done" />
+          <div style={{ display: "inline" }}>
+            <label htmlFor="title">Title</label>{" "}
+            <input type="text" id="title" name="title" placeholder="Title" />
+          </div>{" "}
+          <button type="submit">Add</button>
+        </fieldset>
+      </Form>
+      <ul>
+        {todos.length ? (
+          todos.map((todo) => (
             <li key={todo.id}>
-              <Link to={todo.id}>{todo.title}</Link>
+              <fetcher.Form method="post" style={{ display: "inline" }}>
+                <input
+                  onChange={handleToggle}
+                  type="checkbox"
+                  name="done"
+                  defaultChecked={todo.done}
+                />
+              </fetcher.Form>
+              {todo.title}{" "}
+              <Form method="post" style={{ display: "inline" }}>
+                <input type="hidden" name="id" value={todo.id} />
+                <button name="_action" value="delete" type="submit">
+                  x
+                </button>
+              </Form>
             </li>
-          ))}
-        </ul>
-      </div>
-    </main>
+          ))
+        ) : (
+          <p>No todos</p>
+        )}
+      </ul>
+    </div>
   );
 }
